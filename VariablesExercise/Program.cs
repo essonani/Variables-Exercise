b7efc64b52d191550ac588c5418f1bb489c6fae0
essonani tchakpalla <essonani@hotmail.com>
2023-05-01 18:45:45 -0500

    rsyrtd

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogname = "fido";
+            int age = 5;
+            char nickname = 'f';
+            bool meowed = false;
+            double weight = 65.2;
+            decimal height = 1.6m;
+            Console.WriteLine("my sister dog is called " + dogname);
+            Console.WriteLine("fido is " + age);
+            Console.WriteLine("his nickname is " + nickname);
+            Console.WriteLine("his height is " + height);
+            Console.WriteLine("fido weight is " + weight);
         }
     }
 }
